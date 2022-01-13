287c71440ce52990cfbff94dde50293948326d44
danao413 <57009924+danao413@users.noreply.github.com>
2022-01-13 09:47:48 -0500

    Update post-processing.py

--- harmonic/post-processing.py
+++ harmonic/post-processing.py
@@ -1,2 +1,4 @@
-#this is where you can get the ZPE, other yaml values, and generate thermal properties
+#this will generate the "thermal_properties.yaml" file as well as other files in HA calculation using phonopy
+#this will also give you ZPE and other yaml values
+#can do this either for a single directory or a list of directories
 import yaml
